bfd073ca5599a950009bc58802348449a8ed159e
Rio Kierkels <riokierkels@gmail.com>
2023-04-07 21:11:38 +0200

    Update .config/astronvim/lua/user/plugins/mason-lspconfig.lua

--- dot_config/astronvim/lua/user/plugins/mason-lspconfig.lua
+++ dot_config/astronvim/lua/user/plugins/mason-lspconfig.lua
@@ -1,9 +1,11 @@
 return {
   {
     "williamboman/mason-lspconfig.nvim",
-    ensure_installed = {
-      "lua_ls",
-      "marksman"
+    opts = {
+      ensure_installed = {
+        "lua_ls",
+        "marksman"
+      }
     }
   }
 }
