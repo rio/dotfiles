d5b2de6321fac1619e870f981c0fc1ff062d2fb6
Rio Kierkels <riokierkels@gmail.com>
2023-10-09 13:42:13 +0200

    Update .wezterm.lua

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -1,9 +1,5 @@
 local wezterm = require("wezterm")
 
-if wezterm.config_builder then
-	config = wezterm.config_builder()
-end
-
 function scheme_for_appearance(appearance)
 	if appearance:find("Dark") then
 		return "Catppuccin Mocha"
@@ -12,8 +8,11 @@ function scheme_for_appearance(appearance)
 	end
 end
 
-color_scheme = scheme_for_appearance(wezterm.gui.get_appearance())
+if wezterm.config_builder then
+	config = wezterm.config_builder()
 
-config.hide_tab_bar_if_only_one_tab = true
+	config.color_scheme = scheme_for_appearance(wezterm.gui.get_appearance())
+	config.hide_tab_bar_if_only_one_tab = true
+end
 
 return config
