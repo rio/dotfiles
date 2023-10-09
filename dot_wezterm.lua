35d8b8713ed476fb74292da560dee8d0ef5ab518
Rio Kierkels <riokierkels@gmail.com>
2023-10-09 13:41:00 +0200

    Update .wezterm.lua

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -1,7 +1,19 @@
 local wezterm = require("wezterm")
-local config = {}
 
-config.color_scheme = "Tokyo Night"
+if wezterm.config_builder then
+	config = wezterm.config_builder()
+end
+
+function scheme_for_appearance(appearance)
+	if appearance:find("Dark") then
+		return "Catppuccin Mocha"
+	else
+		return "Catppuccin Latte"
+	end
+end
+
+color_scheme = scheme_for_appearance(wezterm.gui.get_appearance())
+
 config.hide_tab_bar_if_only_one_tab = true
 
 return config
