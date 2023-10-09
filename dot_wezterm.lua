1ed1a90fa81abef062e381b7857279cae9af3e09
Rio Kierkels <riokierkels@gmail.com>
2023-10-09 13:43:48 +0200

    Revert "Add .config/nvim/lua/config/lazy.lua"
    This reverts commit 612179273203cabb23df7bb2c10d96a721636fdc.

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require("wezterm")
 
 function scheme_for_appearance(appearance)
 	if appearance:find("Dark") then
-		return "Catppuccin Macchiato"
+		return "Catppuccin Mocha"
 	else
 		return "Catppuccin Latte"
 	end
