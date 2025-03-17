754a0d1d5102609dd4588aef33a8a8550df2759a
Rio Kierkels <riokierkels@gmail.com>
2025-03-17 09:51:22 +0100

    feat: add copilot config

--- dot_config/nvim/lua/plugins/extras.lua
+++ dot_config/nvim/lua/plugins/extras.lua
@@ -1,8 +1,9 @@
 return {
+  { import = "lazyvim.plugins.extras.ai.copilot" },
+  { import = "lazyvim.plugins.extras.coding.mini-surround" },
   { import = "lazyvim.plugins.extras.lang.json" },
+  { import = "lazyvim.plugins.extras.lang.markdown" },
   { import = "lazyvim.plugins.extras.lang.toml" },
   { import = "lazyvim.plugins.extras.lang.yaml" },
-  { import = "lazyvim.plugins.extras.lang.markdown" },
   { import = "lazyvim.plugins.extras.ui.mini-indentscope" },
-  { import = "lazyvim.plugins.extras.coding.mini-surround" },
 }
